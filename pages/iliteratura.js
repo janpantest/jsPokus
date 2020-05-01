b0ca47d37a5a0bf09f96cf84b3a5ff37c418e2c9
janpantest <krchovaak@gmail.com>
2020-05-01 17:39:54 +0200

    test updated

--- pages/iliteratura.js
+++ pages/iliteratura.js
@@ -3,13 +3,17 @@ const { I } = inject();
 module.exports = {
 
   locators: {
-    titul: "//div/span[./text()='Arikawa, Hiro: Kočka na cestách']"
+    titul: "//div/span[./text()='Robb, Alice: Proč sníme?']",
+    nexPage: "(//a[./text()='2'])[2]",
   },
 
   findTitul() {
     I.seeInTitle('iLiteratura');
+    if (I.dontSeeElement(this.locators.titul)) {
+      I.click(this.locators.nexPage)
+    }
     I.click(this.locators.titul);
-    I.seeInTitle('Hiro');
+    I.seeInTitle('Alice');
   }
 
 
