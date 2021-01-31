d7102c8f64ba1e3c090e736f8788e2100f719b77
janpantest <krchovaak@gmail.com>
2021-01-31 21:23:01 +0100

    googleInputPage added

---
+++ pages/googleInput.js
@@ -0,0 +1,49 @@
+const { I } = inject();
+
+module.exports = {
+  
+  // insert your locators and methods here
+  locators: {
+    agreeButton: "//form/div[@id='introAgreeButton']",
+    searchField: "//input[@name='q']",
+    agreeLabel: "//iframe[contains(@src, 'consent.google.com')",
+    searchOption: "(//div[@class='sbl1'])[1]",
+  },
+
+  acceptAgreement() {
+    I.amOnPage('https://google.com');
+    I.switchTo({xpath: '//iframe[contains(@src, "consent.google.com")]'});
+    I.click({xpath: '//div[@id="introAgreeButton"]'});
+
+    // if (I.seeElement(this.locators.agreeLabel)) {
+    //   I.switchTo(this.locators.agreeLabel)
+    //   I.click(this.locators.agreeButton)
+    // }
+
+  },
+
+  acceptance() {
+    I.amOnPage('https://google.com');
+    I.switchTo(this.locators.agreeLabel);
+    I.switchToPreviousTab(0);
+    // I.click(this.locators.agreeButton);
+  },
+
+  // enterString(string) {
+  //   I.amOnPage('https://www.google.com/');
+  //   this.acceptAgreement();
+  //   I.seeInTitle('Google');
+  //   // I.seeElement(this.locators.searchField);
+  //   // I.fillField(this.locators.searchField);
+  // }
+
+  enterString(string) {
+    I.seeInTitle('Google');
+    I.seeElement(this.locators.searchField);
+    I.fillField(this.locators.searchField, string);
+    I.click(this.locators.searchOption)
+  }
+
+
+}
+
